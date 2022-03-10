d8686fa7863802418d60e1b57fa8f7fd7f60c7fa
silvinovitta <ardhi.kholifatul8@gmail.com>
2022-03-10 11:15:00 +0700

    Praktikum 6

---
+++ JS01/destructuring.js
@@ -0,0 +1,9 @@
+let polStudent = ({name, polytechnic, kerja}) => {
+    alert (`${name} from ${polytechnic} ${kerja}`);
+};
+
+polStudent ({
+    name: 'Yuyun nailufar',
+    polytechnic: 'CBM',
+    kerja : 'semangat kerjanya'
+});
