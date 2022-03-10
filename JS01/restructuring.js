25641c00fa43697809862f37bbbc169dff5b7b11
silvinovitta <ardhi.kholifatul8@gmail.com>
2022-03-10 11:27:29 +0700

    Praktikum 8

---
+++ JS01/restructuring.js
@@ -0,0 +1,9 @@
+var pemainSepakbola = {
+    name: 'ayang',
+    height: '165',
+    
+    output(){
+        alert(`Mrs.${this.name} is ${this.height} centimeter tall`);
+    }
+};
+pemainSepakbola.output();
